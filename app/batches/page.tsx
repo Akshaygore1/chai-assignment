2e6ff6ffd4683ff5b10041fddd1b02c496b1923f
Akshaygore1 <akshaygore2301@gmail.com>
2024-07-23 10:28:11 +0530

    batches page added

---
+++ app/batches/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-10 bg-[#E2BBE9]">
+      <h1 className="text-2xl font-bold">Batches</h1>
+    </div>
+  );
+}
